5de97c08b0313fe6be89c5c0c7229880e82373b1
Ayush Anand <anandayush573@gmail.com>
2025-02-20 21:40:08 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import AddTransaction from "./components/AddTransaction"
 import Balance from "./components/Balance"
-import Header from "./components/header"
+import Header from "./components/Header"
 import IncomeExpense from "./components/IncomeExpense"
 import TransactionList from "./components/TransactionList";
 import {GlobalProvider} from "./context/GlobalState";
